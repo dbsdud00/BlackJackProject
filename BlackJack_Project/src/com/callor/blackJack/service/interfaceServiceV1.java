7acdc02f2643f49e77e972cd9140444745d11cc6
dbsdud00 <dbsdudsw@icloud.com>
2023-05-17 19:27:44 +0900

    05

--- BlackJack_Project/src/com/callor/blackJack/service/interfaceServiceV1.java
+++ BlackJack_Project/src/com/callor/blackJack/service/interfaceServiceV1.java
@@ -7,39 +7,59 @@ public class interfaceServiceV1 implements interfaceService {
 	@Override
 	public void gameStart() {
 		// TODO Auto-generated method stub
-		int front = 15;
-		int back = 15;
-		int middle = 31-front-back;
-		System.out.println("*".repeat(31));
-		System.out.println("*".repeat(31));
+//		int front = 15;
+//		int back = 15;
+//		int middle = 31-front-back;
+//		System.out.println("*".repeat(31));
+//		System.out.println("*".repeat(31));
+//		
+//		for (int i = 0;i< 8 ; i++) {
+//			System.out.print("*".repeat(front));
+//			System.out.print(" ".repeat(middle));
+//			System.out.print("*".repeat(back));
+//			front--;
+//			back--;
+//			middle = 31-front-back;
+//			System.out.println();
+//		}
+//		for (int i = 0;i< 8 ; i++) {
+//			front++;
+//			back++;
+//			middle = 31-front-back;
+//			System.out.print("*".repeat(front));
+//			System.out.print(" ".repeat(middle));
+//			System.out.print("*".repeat(back));
+//			System.out.println();
+//		}
+//		System.out.println("*".repeat(31));
+//		System.out.println("*".repeat(31));
+//		System.out.println();
 		
-		for (int i = 0;i< 8 ; i++) {
-			System.out.print("*".repeat(front));
-			System.out.print(" ".repeat(middle));
-			System.out.print("*".repeat(back));
-			front--;
-			back--;
-			middle = 31-front-back;
-			System.out.println();
-		}
-		for (int i = 0;i< 8 ; i++) {
-			front++;
-			back++;
-			middle = 31-front-back;
-			System.out.print("*".repeat(front));
-			System.out.print(" ".repeat(middle));
-			System.out.print("*".repeat(back));
-			System.out.println();
-		}
-		System.out.println("*".repeat(31));
-		System.out.println("*".repeat(31));
+		
+		System.out.println("*".repeat(81));
+		System.out.println("*\t@@@@   @       @@@    @@@@  @   @     @@@@@   @@@    @@@@  @   @\t*");
+		System.out.println("*\t@   @  @      @   @  @      @  @        @    @   @  @      @  @ \t*");
+		System.out.println("*\t@   @  @      @   @  @      @ @         @    @   @  @      @ @  \t*");
+		System.out.println("*\t@@@@   @      @@@@@  @      @@          @    @@@@@  @      @@   \t*");
+		System.out.println("*\t@   @  @      @   @  @      @ @         @    @   @  @      @ @  \t*");
+		System.out.println("*\t@   @  @      @   @  @      @  @        @    @   @  @      @  @ \t*"); 
+		System.out.println("*\t@@@@   @@@@@  @   @   @@@@  @   @     @@@    @   @   @@@@  @   @\t*");
+		System.out.println("*".repeat(81));
+		
+		System.out.println("@".repeat(81));
+		System.out.println("@@@@@@@@    @@@ @@@@@@@   @@@@    @@ @@@ @@@@@     @@@   @@@@    @@ @@@@ @@@@@@@@");
+		System.out.println("@@@@@@@@ @@@ @@ @@@@@@ @@@ @@ @@@@@@ @@ @@@@@@@@ @@@@ @@@ @@ @@@@@@ @@  @@@@@@@@@");
+		System.out.println("@@@@@@@@ @@@ @@ @@@@@@ @@@ @@ @@@@@@ @ @@@@@@@@@ @@@@ @@@ @@ @@@@@@ @ @@@@@@@@@@@");
+		System.out.println("@@@@@@@@    @@@ @@@@@@     @@ @@@@@@  @@@@@@@@@@ @@@@     @@ @@@@@@  @@@@@@@@@@@@");
+		System.out.println("@@@@@@@@ @@@ @@ @@@@@@ @@@ @@ @@@@@@ @ @@@@@@@@@ @@@@ @@@ @@ @@@@@@ @ @@@@@@@@@@@");
+		System.out.println("@@@@@@@@ @@@ @@ @@@@@@ @@@ @@ @@@@@@ @@ @@@@@@@@ @@@@ @@@ @@ @@@@@@ @@ @@@@@@@@@@"); 
+		System.out.println("@@@@@@@@    @@@     @@ @@@ @@@    @@ @@@ @@@@@   @@@@ @@@ @@@    @@ @@@ @@@@@@@@@");
+		System.out.println("@".repeat(81));
 		System.out.println();
 		
 		
 		
-		System.out.println("***  *     **   *** *  * ****  **   *** *  *");
-		System.out.println("*  * *    *  * *    * *    *  *  * *    * * ");
-		System.out.println("*  * * ");
+		
 		
 		
 		
@@ -53,7 +73,7 @@ public class interfaceServiceV1 implements interfaceService {
 			inGame = new InGameV3();
 			inGame.gameStart();
 			System.out.println("\n\n\n");
-			System.out.print("게임을 계속하시겠습니까? (Y/N) >> ");
+			System.out.print("게임을 계속하시겠습니까? (Y/N) >> "); 
 			String YN = scan.nextLine();
 			if (YN.equals("N")) {
 				break;
